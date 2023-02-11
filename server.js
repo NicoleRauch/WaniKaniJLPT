a85e52081d672c8590b7bfc83ed24adc300def98
Nicole Rauch <info@nicole-rauch.de>
2023-02-11 21:59:03 +0100

    Data is now kept in the script. Server is only used for prepping the data.

--- server.js
+++ server.js
@@ -5,11 +5,13 @@
 // BASE SETUP
 // =============================================================================
 
+
 // load the packages we need
-var express = require('express');
-var bodyParser = require('body-parser');
 var fs = require('fs');
 var path = require('path');
+/*
+var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();                 // define our app using express
 
@@ -29,59 +31,67 @@ app.use(function (req, res, next) {
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
+*/
 
 // set up the data to be used in the API:
 
-const n5tag = "jlptn5";
-const n4tag = "jlptn4";
-const n3tag = "jlptn3";
-const n2tag = "jlptn2";
-const n1tag = "jlptn1";
+const n5tag = 5;
+const n4tag = 4;
+const n3tag = 3;
+const n2tag = 2;
+const n1tag = 1;
 
 const levelMap = {};
 
-var JLPTN5Vocab = path.join(__dirname, 'n5-vocab-kanji-hiragana.txt');
-var JLPTN4Vocab = path.join(__dirname, 'n4-vocab-kanji-hiragana.txt');
-var JLPTN3Vocab = path.join(__dirname, 'n3-vocab-kanji-hiragana.txt');
-var JLPTN2Vocab = path.join(__dirname, 'n2-vocab-kanji-hiragana.txt');
-var JLPTN1Vocab = path.join(__dirname, 'n1-vocab-kanji-hiragana.txt');
+var JLPTN5Vocab = path.join(__dirname, 'data/n5-vocab-kanji-hiragana.txt');
+var JLPTN4Vocab = path.join(__dirname, 'data/n4-vocab-kanji-hiragana.txt');
+var JLPTN3Vocab = path.join(__dirname, 'data/n3-vocab-kanji-hiragana.txt');
+var JLPTN2Vocab = path.join(__dirname, 'data/n2-vocab-kanji-hiragana.txt');
+var JLPTN1Vocab = path.join(__dirname, 'data/n1-vocab-kanji-hiragana.txt');
 
-var JLPTN5Kanji = path.join(__dirname, 'n5-kanji.txt');
-var JLPTN4Kanji = path.join(__dirname, 'n4-kanji.txt');
-var JLPTN3Kanji = path.join(__dirname, 'n3-kanji.txt');
-var JLPTN2Kanji = path.join(__dirname, 'n2-kanji.txt');
-var JLPTN1Kanji = path.join(__dirname, 'n1-kanji.txt');
+var JLPTN5Kanji = path.join(__dirname, 'data/n5-kanji.txt');
+var JLPTN4Kanji = path.join(__dirname, 'data/n4-kanji.txt');
+var JLPTN3Kanji = path.join(__dirname, 'data/n3-kanji.txt');
+var JLPTN2Kanji = path.join(__dirname, 'data/n2-kanji.txt');
+var JLPTN1Kanji = path.join(__dirname, 'data/n1-kanji.txt');
 
 function insertVocabData(file, levelTag) {
+    console.log("vocab " + levelTag)
     var fileContents = fs.readFileSync(file, {encoding: "utf8"});
     fileContents.split("\n").forEach(row => {
         const word = row.split("\t");
         levelMap[word[0] + " " + word[1]] = levelTag;
-        console.log(word[0] + " " + word[1] + " " + levelTag);
+        // console.log(word[0] + " " + word[1] + " " + levelTag);
     });
 }
 
 function insertKanjiData(file, levelTag) {
+    console.log("kanji " + levelTag)
     var fileContents = fs.readFileSync(file, {encoding: "utf8"});
     fileContents.split("\n").forEach(row => {
         const word = row.split("\t");
         levelMap[word[0]] = levelTag;
-        console.log(word[0] + " " + levelTag);
+        // console.log(word[0] + " " + levelTag);
     });
 }
 
-insertVocabData(JLPTN5Vocab, n5tag);
-insertVocabData(JLPTN4Vocab, n4tag);
-insertVocabData(JLPTN3Vocab, n3tag);
-insertVocabData(JLPTN2Vocab, n2tag);
 insertVocabData(JLPTN1Vocab, n1tag);
+insertVocabData(JLPTN2Vocab, n2tag);
+insertVocabData(JLPTN3Vocab, n3tag);
+insertVocabData(JLPTN4Vocab, n4tag);
+insertVocabData(JLPTN5Vocab, n5tag);
 
-insertKanjiData(JLPTN5Kanji, n5tag);
-insertKanjiData(JLPTN4Kanji, n4tag);
-insertKanjiData(JLPTN3Kanji, n3tag);
-insertKanjiData(JLPTN2Kanji, n2tag);
 insertKanjiData(JLPTN1Kanji, n1tag);
+insertKanjiData(JLPTN2Kanji, n2tag);
+insertKanjiData(JLPTN3Kanji, n3tag);
+insertKanjiData(JLPTN4Kanji, n4tag);
+insertKanjiData(JLPTN5Kanji, n5tag);
 
+delete levelMap[" undefined"]
+
+fs.writeFileSync("result.txt", JSON.stringify(levelMap))
+
+/*
 router.get('/', function (req, res, next) {
     res.send("Willkommen zur JLPT Server API!")
 });
@@ -138,3 +148,5 @@ app.use('/', hello);
 app.listen(app.get('port'), function () {
     console.log('Connect to the server via http://localhost:' + app.get('port'));
 });
+
+*/
